b14f7103321574717617f8a6918b62fafdc170cf
VrublevskiViktor <VrublevskiViktor@outlook.com>
2023-09-08 22:56:31 +0300

    New2

--- TestProjectPrism/App.xaml.cs
+++ TestProjectPrism/App.xaml.cs
@@ -29,7 +29,5 @@ namespace TestProjectPrism
             moduleCatalog.AddModule(typeof(UIModule.UIModule));
             moduleCatalog.AddModule(typeof(DialogModule.DialogModule));
         }
-
-
     }
 }
